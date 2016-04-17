d0a8ec456f4fc7a1572aa67ef548bdc4f629e0ad
Vincent Huang <vincom2@gmail.com>
2016-04-17 16:25:30 -0400

    fix obvious stuff

--- src/transaction/tobject.rs
+++ src/transaction/tobject.rs
@@ -9,17 +9,17 @@ use super::res::*;
 static GLOBAL_SEQ_LOCK: AtomicUsize = ATOMIC_USIZE_INIT;
 
 pub struct Transaction {
-    usize: snapshot
-    writeSet: HashMap<&mut TVar<T>, u32>
-    readSet: Vec<(&mut TVar<T>, u32)>
+    usize: snapshot,
+    writeSet: HashMap<&mut TVar<T>, u32>,
+    readSet: Vec<(&mut TVar<T>, u32)>,
 }
 
 impl Transaction {
     fn new(ss: usize) -> Transaction {
         Transaction {
-            snapShot: ss
-            writeSet: BTreeMap::new()
-            readSet: Vec::new()
+            snapShot: ss,
+            writeSet: BTreeMap::new(),
+            readSet: Vec::new(),
         }
     }
 
@@ -27,7 +27,7 @@ impl Transaction {
     where F: Fn(&mut Transaction) -> StmResult<T>,
     {
         let mut ss = GLOBAL_SEQ_LOCK.load(Ordering::SeqCst);
-        while ((ss & 1) != 0) {
+        while (ss & 1) != 0 {
             ss = GLOBAL_SEQ_LOCK.load(Ordering::SeqCst);
         }
         let mut transaction = Transaction::new(ss);
@@ -46,14 +46,14 @@ impl Transaction {
 
     pub fn read<T>(&mut self, var: &mut TVar<T>) -> StmResult<T> {
         match self.writeSet.get(&var) {
-            Some &val => return Ok(val)
+            Some(&val) => { return Ok(val) }
             None => {}
         };
         let mut val = (*var).value;
-        while (self.snapshot != GLOBAL_SEQ_LOCK.load(Ordering::SeqCst)) {
-            self.snapshot = self.validate()
+        while self.snapshot != GLOBAL_SEQ_LOCK.load(Ordering::SeqCst) {
+            self.snapshot = self.validate();
             val = (*var).value;
-            if (self.snapshot < 0) {
+            if self.snapshot < 0 {
                 return Err(Retry);
             }
         }
@@ -72,30 +72,30 @@ impl Transaction {
         self.writeSet.clear();
     }
 
-    fn validate(&mut self) -> <usize> {
+    fn validate(&mut self) -> usize {
         loop {
-            let mut usize time = GLOBAL_SEQ_LOCK.load(Ordering::SeqCst);
-            if ((time & 1) != 0) {
+            let mut time : usize = GLOBAL_SEQ_LOCK.load(Ordering::SeqCst);
+            if (time & 1) != 0 {
                 continue;
             }
             for (addr, val) in self.readSet {
-                if ((*addr).value != val) {
+                if (*addr).value != val {
                     return -1;
                 }
             }
-            if (time == lock.load(Ordering::SeqCst)) {
+            if time == lock.load(Ordering::SeqCst) {
                 return time;
             }
         }
     }
 
     fn commit(&mut self) -> bool {
-        if (self.writeSet.len() == 0) {
+        if self.writeSet.len() == 0 {
             return true;
         }
-        while (GLOBAL_SEQ_LOCK.compare_and_swap.(self.snapshot, self.snapshot +  1, Ordering::SeqCst) != self.snapshot) {
-            self.snapshot = self.validate()
-            if (self.snapshot < 0) {
+        while GLOBAL_SEQ_LOCK.compare_and_swap(self.snapshot, self.snapshot+1, Ordering::SeqCst) != self.snapshot {
+            self.snapshot = self.validate();
+            if self.snapshot < 0 {
                 return false;
             }
         }
@@ -106,3 +106,4 @@ impl Transaction {
         return true;
     }
 }
+
